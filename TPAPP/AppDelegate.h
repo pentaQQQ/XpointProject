351c7f985d5bdf87af452248954608f76dfd09e0
fudong_zhang <fudong_zhang@idea-source.net>
2017-07-18 10:23:45 +0800

    Initial Commit

---
+++ TPAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TPAPP
+//
+//  Created by 上海点硕 on 2017/7/18.
+//  Copyright © 2017年 cbl－　点硕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TPAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TPAPP
+//
+//  Created by 上海点硕 on 2017/7/18.
+//  Copyright © 2017年 cbl－　点硕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
