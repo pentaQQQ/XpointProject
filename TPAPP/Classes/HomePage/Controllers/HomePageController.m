aea2e0113082de787f778dca227af3548b30aef1
崔文龙 <cuiwenlong@cuiwenlongdeMacBook-Pro.local>
2018-08-20 18:10:28 +0800

    首页界面的部分搭建

--- TPAPP/Classes/HomePage/Controllers/HomePageController.m
+++ TPAPP/Classes/HomePage/Controllers/HomePageController.m
@@ -7,17 +7,82 @@
 //hshfljdhkjashkad
 
 #import "HomePageController.h"
-
-@interface HomePageController ()
+#import "SGPagingView.h"
+#import "ClassDetailViewController.h"
+@interface HomePageController ()<SGPageTitleViewDelegate, SGPageContentScrollViewDelegate>
+@property (nonatomic, strong) SGPageTitleView *pageTitleView;
+@property (nonatomic, strong) SGPageContentScrollView *pageContentScrollView;
 
 @end
 
 @implementation HomePageController
 
+- (void)dealloc {
+    NSLog(@"DefaultScrollVC - - dealloc");
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor orangeColor];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeSelectedIndex:) name:@"changeSelectedIndex" object:nil];
+    
+    [self setupPageView];
 }
+
+- (void)changeSelectedIndex:(NSNotification *)noti {
+    _pageTitleView.resetSelectedIndex = [noti.object integerValue];
+}
+
+- (void)setupPageView {
+    CGFloat statusHeight = CGRectGetHeight([UIApplication sharedApplication].statusBarFrame);
+    CGFloat pageTitleViewY = 0;
+    if (statusHeight == 20.0) {
+        pageTitleViewY = 64;
+    } else {
+        pageTitleViewY = 88;
+    }
+    
+    NSArray *titleArr = @[@"精选", @"电影", @"电视剧", @"综艺", @"NBA", @"娱乐", @"动漫", @"演唱会", @"VIP会员"];
+    SGPageTitleViewConfigure *configure = [SGPageTitleViewConfigure pageTitleViewConfigure];
+    configure.indicatorAdditionalWidth = 10; // 说明：指示器额外增加的宽度，不设置，指示器宽度为标题文字宽度；若设置无限大，则指示器宽度为按钮宽度
+    configure.showBottomSeparator = NO;
+    
+    /// pageTitleView
+    self.pageTitleView = [SGPageTitleView pageTitleViewWithFrame:CGRectMake(0, pageTitleViewY, self.view.frame.size.width, 44) delegate:self titleNames:titleArr configure:configure];
+    [self.view addSubview:_pageTitleView];
+    [_pageTitleView addBadgeForIndex:1];
+    [_pageTitleView addBadgeForIndex:5];
+    
+    NSMutableArray *childArr = [NSMutableArray array];
+    for (int i=0; i<titleArr.count; i++) {
+        ClassDetailViewController *vc = [[ClassDetailViewController alloc]init];
+        [childArr addObject:vc];
+    }
+    
+    CGFloat ContentCollectionViewHeight = self.view.frame.size.height - CGRectGetMaxY(_pageTitleView.frame);
+    self.pageContentScrollView = [[SGPageContentScrollView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_pageTitleView.frame), self.view.frame.size.width, ContentCollectionViewHeight) parentVC:self childVCs:childArr];
+    _pageContentScrollView.delegatePageContentScrollView = self;
+    [self.view addSubview:_pageContentScrollView];
+}
+
+- (void)pageTitleView:(SGPageTitleView *)pageTitleView selectedIndex:(NSInteger)selectedIndex {
+    [self.pageContentScrollView setPageContentScrollViewCurrentIndex:selectedIndex];
+}
+
+- (void)pageContentScrollView:(SGPageContentScrollView *)pageContentScrollView progress:(CGFloat)progress originalIndex:(NSInteger)originalIndex targetIndex:(NSInteger)targetIndex {
+    [self.pageTitleView setPageTitleViewWithProgress:progress originalIndex:originalIndex targetIndex:targetIndex];
+}
+
+- (void)pageContentScrollView:(SGPageContentScrollView *)pageContentScrollView index:(NSInteger)index {
+    if (index == 1 || index == 5) {
+        [_pageTitleView removeBadgeForIndex:index];
+    }
+}
+
 
 
 //网络请求实列
---
+++ TPAPP/Classes/HomePage/view/goodsDetailCell.m
@@ -0,0 +1,24 @@
+//
+//  goodsDetailCell.m
+//  TPAPP
+//
+//  Created by 崔文龙 on 2018/8/20.
+//  Copyright © 2018年 cbl－　点硕. All rights reserved.
+//
+
+#import "goodsDetailCell.h"
+
+@implementation goodsDetailCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
